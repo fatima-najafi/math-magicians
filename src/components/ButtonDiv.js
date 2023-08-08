3eb78021b47d5e0ac538210351333b567b40b874
Fatima Najafi <najafifatima762@gmail.com>
2023-08-08 16:13:12 +0430

    add new file for buttons

---
+++ src/components/ButtonDiv.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/button-has-type */
+
+function ButtonDiv() {
+  return (
+    <div className="parent">
+      <button className="button">AC</button>
+      <button className="button">+/-</button>
+      <button className="button">%</button>
+      <button className="button-color">/</button>
+      <button className="button">7</button>
+      <button className="button">8</button>
+      <button className="button">9</button>
+      <button className="button-color">X</button>
+      <button className="button">4</button>
+      <button className="button">5</button>
+      <button className="button">6</button>
+      <button className="button-color">-</button>
+      <button className="button">1</button>
+      <button className="button">2</button>
+      <button className="button">3</button>
+      <button className="button-color">+</button>
+      <button className="button-zero">0</button>
+      <button className="button">.</button>
+      <button className="button-color">=</button>
+    </div>
+  );
+}
+export default ButtonDiv;
