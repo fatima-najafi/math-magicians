27f1ff37f36de9f65483cc17f992a3e1b80459c6
Fatima Najafi <najafifatima762@gmail.com>
2023-08-08 16:52:51 +0430

    fixed linters errors

--- src/components/ButtonDiv.js
+++ src/components/ButtonDiv.js
@@ -1,27 +1,63 @@
-/* eslint-disable react/button-has-type */
-
 function ButtonDiv() {
   return (
     <div className="parent">
-      <button className="button">AC</button>
-      <button className="button">+/-</button>
-      <button className="button">%</button>
-      <button className="button-color">/</button>
-      <button className="button">7</button>
-      <button className="button">8</button>
-      <button className="button">9</button>
-      <button className="button-color">X</button>
-      <button className="button">4</button>
-      <button className="button">5</button>
-      <button className="button">6</button>
-      <button className="button-color">-</button>
-      <button className="button">1</button>
-      <button className="button">2</button>
-      <button className="button">3</button>
-      <button className="button-color">+</button>
-      <button className="button-zero">0</button>
-      <button className="button">.</button>
-      <button className="button-color">=</button>
+      <button type="button" className="button">
+        AC
+      </button>
+      <button type="button" className="button">
+        +/-
+      </button>
+      <button type="button" className="button">
+        %
+      </button>
+      <button type="button" className="button-color">
+        /
+      </button>
+      <button type="button" className="button">
+        7
+      </button>
+      <button type="button" className="button">
+        8
+      </button>
+      <button type="button" className="button">
+        9
+      </button>
+      <button type="button" className="button-color">
+        X
+      </button>
+      <button type="button" className="button">
+        4
+      </button>
+      <button type="button" className="button">
+        5
+      </button>
+      <button type="button" className="button">
+        6
+      </button>
+      <button type="button" className="button-color">
+        -
+      </button>
+      <button type="button" className="button">
+        1
+      </button>
+      <button type="button" className="button">
+        2
+      </button>
+      <button type="button" className="button">
+        3
+      </button>
+      <button type="button" className="button-color">
+        +
+      </button>
+      <button type="button" className="button-zero">
+        0
+      </button>
+      <button type="button" className="button">
+        .
+      </button>
+      <button type="button" className="button-color">
+        =
+      </button>
     </div>
   );
 }
