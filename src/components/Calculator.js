96998590fd54013cd94135cd85ceb839fd17dd4d
Fatima Najafi <najafifatima762@gmail.com>
2023-08-08 16:13:55 +0430

    import the buttonDiv file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,8 @@
-/* eslint-disable react/button-has-type */
 import React from 'react';
 import './Calculator.css';
+import ButtonDiv from './ButtonDiv';
 
 function Calculator() {
-  function ButtonDiv() {
-    return (
-      <div className="parent">
-        <button className="button">AC</button>
-        <button className="button">+/-</button>
-        <button className="button">%</button>
-        <button className="button-color">/</button>
-        <button className="button">7</button>
-        <button className="button">8</button>
-        <button className="button">9</button>
-        <button className="button-color">X</button>
-        <button className="button">4</button>
-        <button className="button">5</button>
-        <button className="button">6</button>
-        <button className="button-color">-</button>
-        <button className="button">1</button>
-        <button className="button">2</button>
-        <button className="button">3</button>
-        <button className="button-color">+</button>
-        <button className="button-zero">0</button>
-        <button className="button">.</button>
-        <button className="button-color">=</button>
-      </div>
-    );
-  }
   return (
     <div className="calculator">
       <div>
