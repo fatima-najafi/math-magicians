e3817e0f1739eab092ba7b7cf9262d7e32847ea9
Fatima Najafi <najafifatima762@gmail.com>
2023-08-09 15:52:29 +0430

    Add event for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,14 +1,85 @@
-/* eslint-disable react/button-has-type */
-import React from 'react';
-import './Calculator.css';
-import ButtonDiv from './ButtonDiv';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
+import './calculator.css';
 
 function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const { total, operation, next } = state;
+  const handleEvent = (e) => {
+    const value = e.target.textContent;
+    const calculat = calculate(state, value);
+    setState(calculat);
+  };
   return (
     <div className="calculator">
-      <div>
-        <input type="text" className="input" id="name" placeholder="0" />
-        <ButtonDiv />
+      <div className="display">
+
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="parent">
+        <button type="button" className="button" onClick={handleEvent}>
+          AC
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          +/-
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          %
+        </button>
+        <button type="button" className="button-color" onClick={handleEvent}>
+          /
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          7
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          8
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          9
+        </button>
+        <button type="button" className="button-color" onClick={handleEvent}>
+          *
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          4
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          5
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          6
+        </button>
+        <button type="button" className="button-color" onClick={handleEvent}>
+          -
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          1
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          2
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          3
+        </button>
+        <button type="button" className="button-color" onClick={handleEvent}>
+          +
+        </button>
+        <button type="button" className="button-zero" onClick={handleEvent}>
+          0
+        </button>
+        <button type="button" className="button" onClick={handleEvent}>
+          .
+        </button>
+        <button type="button" className="button-color" onClick={handleEvent}>
+          =
+        </button>
       </div>
     </div>
   );
