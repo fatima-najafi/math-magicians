58e0feba46008d5e74d6d9b5bc095162f89817bf
Fatima Najafi <najafifatima762@gmail.com>
2023-08-16 17:07:00 +0430

    Update the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,19 @@ import './Style.css';
 
 function Navbar() {
   return (
-    <div className="container">
+    <div className="container" data-testid="navbar">
       <div>
         <h1>Math Magicians</h1>
       </div>
       <div className="navbar">
-        <div className="nav-item">
+        <div className="nav-item" data-testid="li">
           <Link to="/">Home</Link>
         </div>
-        <div className="nav-item">
+        <div className="nav-item" data-testid="li">
           <span className="nav-item">|</span>
           <Link to="/calculator">Calculator</Link>
         </div>
-        <div className="nav-item">
+        <div className="nav-item" data-testid="li">
           <span className="nav-item">|</span>
           <Link to="/quote">Quote</Link>
         </div>
