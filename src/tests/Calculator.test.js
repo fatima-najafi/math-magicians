ed0881acc1b153addd3ac45cf9268009ea509520
Fatima Najafi <najafifatima762@gmail.com>
2023-08-16 19:01:16 +0430

    Update accroding  the request changes

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -4,7 +4,7 @@ import Calculator from '../components/Calculator';
 describe('render the calculator', () => {
   test('displays the calculator text', () => {
     const { container } = render(<Calculator />);
-    const textCalculator = screen.getByText(/Lets doing some math!/i);
+    const textCalculator = screen.getByText(/Lets do some math!/i);
     expect(textCalculator).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
